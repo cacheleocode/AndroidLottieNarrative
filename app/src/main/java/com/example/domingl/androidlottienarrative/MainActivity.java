2d90106754749bafa252364f60faae1f10f2e6cc
Leo Dominguez <Leo@LeoDominguez.com>
2017-10-10 15:44:09 -0700

    actual listeners

--- app/src/main/java/com/example/domingl/androidlottienarrative/MainActivity.java
+++ app/src/main/java/com/example/domingl/androidlottienarrative/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.domingl.androidlottienarrative;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
 import android.animation.ValueAnimator;
 import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 
 import com.airbnb.lottie.LottieAnimationView;
 
@@ -16,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     private LottieAnimationView animationViewPairingLoop;
     private LottieAnimationView animationViewPairingEnd;
     private LottieAnimationView animationViewLocationStart;
+    private LottieAnimationView animationViewSearchingStart;
+    private LottieAnimationView animationViewSearchingLoop;
+    private LottieAnimationView animationViewSearchingEnd;
 
     private Integer step = 0;
     private Boolean success = false;
@@ -41,14 +49,27 @@ public class MainActivity extends AppCompatActivity {
         animationViewRemoteStart.playAnimation();
         step = 1;
 
-        animationViewRemoteStart.addAnimatorUpdateListener(new AnimatorUpdateListener() {
+        animationViewRemoteStart.addAnimatorListener(new AnimatorListener() {
             @Override
-            public void onAnimationUpdate(ValueAnimator animator) {
-                if (animator.getAnimatedFraction() == 1.0) {
-                    animationViewRemoteStart.setVisibility(View.INVISIBLE);
-                    animationViewRemoteLoop.setVisibility(View.VISIBLE);
-                    animationViewRemoteLoop.playAnimation();
-                }
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationViewRemoteStart.setVisibility(View.INVISIBLE);
+                animationViewRemoteLoop.setVisibility(View.VISIBLE);
+                animationViewRemoteLoop.playAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
             }
         });
 
@@ -56,18 +77,55 @@ public class MainActivity extends AppCompatActivity {
         animationViewRemoteLoop.setAnimation("remote_loop.json");
         animationViewRemoteLoop.loop(true);
 
+        animationViewRemoteLoop.addAnimatorListener(new AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationViewRemoteLoop.setVisibility(View.INVISIBLE);
+                animationViewPairingStart.setVisibility(View.VISIBLE);
+                animationViewPairingStart.playAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
         animationViewPairingStart = findViewById(R.id.animation_view_pairing_start);
         animationViewPairingStart.setAnimation("pairing_start.json");
         animationViewPairingStart.loop(false);
 
-        animationViewPairingStart.addAnimatorUpdateListener(new AnimatorUpdateListener() {
+        animationViewPairingStart.addAnimatorListener(new AnimatorListener() {
             @Override
-            public void onAnimationUpdate(ValueAnimator animator) {
-                if (animator.getAnimatedFraction() == 1.0) {
-                    animationViewPairingStart.setVisibility(View.INVISIBLE);
-                    animationViewPairingLoop.setVisibility(View.VISIBLE);
-                    animationViewPairingLoop.playAnimation();
-                }
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationViewPairingStart.setVisibility(View.INVISIBLE);
+                animationViewPairingLoop.setVisibility(View.VISIBLE);
+                animationViewPairingLoop.playAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
             }
         });
 
@@ -75,47 +133,167 @@ public class MainActivity extends AppCompatActivity {
         animationViewPairingLoop.setAnimation("pairing_loop.json");
         animationViewPairingLoop.loop(true);
 
+        animationViewPairingLoop.addAnimatorListener(new AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationViewPairingLoop.setVisibility(View.INVISIBLE);
+                animationViewPairingEnd.setVisibility(View.VISIBLE);
+                animationViewPairingEnd.playAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
         animationViewPairingEnd = findViewById(R.id.animation_view_pairing_end);
         animationViewPairingEnd.setAnimation("pairing_end.json");
         animationViewPairingEnd.loop(false);
 
-        animationViewPairingEnd.addAnimatorUpdateListener(new AnimatorUpdateListener() {
+        animationViewPairingEnd.addAnimatorListener(new AnimatorListener() {
             @Override
-            public void onAnimationUpdate(ValueAnimator animator) {
-                if (animator.getAnimatedFraction() == 1.0) {
-                    animationViewPairingEnd.setVisibility(View.INVISIBLE);
-                    animationViewLocationStart.setVisibility(View.VISIBLE);
-                    animationViewLocationStart.playAnimation();
-                }
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationViewPairingEnd.setVisibility(View.INVISIBLE);
+                animationViewLocationStart.setVisibility(View.VISIBLE);
+                animationViewLocationStart.playAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
             }
         });
 
         animationViewLocationStart = findViewById(R.id.animation_view_location_start);
         animationViewLocationStart.setAnimation("location_start.json");
         animationViewLocationStart.loop(false);
+
+        animationViewLocationStart.addAnimatorListener(new AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationViewLocationStart.setVisibility(View.INVISIBLE);
+                animationViewSearchingStart.setVisibility(View.VISIBLE);
+                animationViewSearchingStart.playAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
+        animationViewSearchingStart = findViewById(R.id.animation_view_searching_start);
+        animationViewSearchingStart.setAnimation("searching_start.json");
+        animationViewSearchingStart.loop(false);
+
+        animationViewSearchingStart.addAnimatorListener(new AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationViewSearchingStart.setVisibility(View.INVISIBLE);
+                animationViewSearchingLoop.setVisibility(View.VISIBLE);
+                animationViewSearchingLoop.playAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
+        animationViewSearchingLoop = findViewById(R.id.animation_view_searching_loop);
+        animationViewSearchingLoop.setAnimation("searching_loop.json");
+        animationViewSearchingLoop.loop(true);
+
+        animationViewSearchingLoop.addAnimatorListener(new AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationViewSearchingLoop.setVisibility(View.INVISIBLE);
+                animationViewSearchingEnd.setVisibility(View.VISIBLE);
+                animationViewSearchingEnd.playAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
+        animationViewSearchingEnd = findViewById(R.id.animation_view_searching_end);
+        animationViewSearchingEnd.setAnimation("searching_end.json");
+        animationViewSearchingEnd.loop(false);
+
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         switch (keyCode) {
             case KeyEvent.KEYCODE_DPAD_LEFT:
+
                 return true;
             case KeyEvent.KEYCODE_DPAD_RIGHT:
 
                 return true;
             case KeyEvent.KEYCODE_DPAD_CENTER:
+                if (!animationViewRemoteStart.isAnimating() && animationViewRemoteStart.getVisibility() == 0) {
+                    animationViewRemoteStart.playAnimation();
+                }
+
                 if (animationViewRemoteLoop.isAnimating()) {
                     animationViewRemoteLoop.addAnimatorUpdateListener(new AnimatorUpdateListener() {
                         @Override
                         public void onAnimationUpdate(ValueAnimator animator) {
                             animator.setRepeatCount(1);
-
-                            if (animator.getAnimatedFraction() == 1.0) {
-                                animationViewRemoteLoop.setVisibility(View.INVISIBLE);
-                                animationViewRemoteLoop.pauseAnimation();
-                                animationViewPairingStart.setVisibility(View.VISIBLE);
-                                animationViewPairingStart.playAnimation();
-                            }
                         }
                     });
                 }
@@ -124,14 +302,16 @@ public class MainActivity extends AppCompatActivity {
                     animationViewPairingLoop.addAnimatorUpdateListener(new AnimatorUpdateListener() {
                         @Override
                         public void onAnimationUpdate(ValueAnimator animator) {
-                            animator.setRepeatCount(1);
+                            animator.setRepeatCount(2);
+                        }
+                    });
+                }
 
-                            if (animator.getAnimatedFraction() == 1.0) {
-                                animationViewPairingLoop.setVisibility(View.INVISIBLE);
-                                animationViewPairingLoop.pauseAnimation();
-                                animationViewPairingEnd.setVisibility(View.VISIBLE);
-                                animationViewPairingEnd.playAnimation();
-                            }
+                if (animationViewSearchingLoop.isAnimating()) {
+                    animationViewSearchingLoop.addAnimatorUpdateListener(new AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animator) {
+                            animator.setRepeatCount(1);
                         }
                     });
                 }
